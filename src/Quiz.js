6908add899d04b880b4a5cda23c499a445dd0a24
Mauricio Castro Rueda <mauricio.castro@contractors.accruent.com>
2018-09-17 13:51:17 -0500

    step 2
    step 2

---
+++ src/Quiz.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react'
+
+let quiz_data = require ('./quiz_data.json')
+
+class Quiz extends Component{
+    constructor(props){
+        super(props)
+        this.state = { QuizQuestion: 1}
+    }
+    render(){
+        return (
+            <div><div className="QuizQuestion">{quiz_data.quiz_questions[0].instruction_text}</div></div>
+        )
+    }
+
+}
+
+export default Quiz
